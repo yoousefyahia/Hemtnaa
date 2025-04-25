dd79f470cfcd4d83f81fbe04e76e9f70301dd762
yousef yahia <yoousefyahia@gmail.com>
2025-04-25 23:49:04 +0300

    up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Home = () => (
   <div className="container d-flex justify-content-center align-items-center min-vh-100">
-    <p className="text-danger fs-5 text-center">
+    <p className="text-danger  text-center">
       جاري العمل على البيانات ... <br />
       وسيكون هذا الجزء متاح قريبًا
     </p>
